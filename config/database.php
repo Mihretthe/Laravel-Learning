eed0bee7b6a0b1f922d50b024d144179377ac842
Mihretthe <mihretekalgn@gmail.com>
2026-04-07 08:59:20 +0300

    Add Tenant model, controller, requests, policy, factory, migration, and seeder for multi-tenant support

--- config/database.php
+++ config/database.php
@@ -49,7 +49,8 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            
+            'database' => env('DB_DATABASE', 'central_db'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -113,6 +114,14 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
+
+        'tenant' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'database' => null,
+            'username' => null,
+            'password' => null,
+        ]
 
     ],
 
