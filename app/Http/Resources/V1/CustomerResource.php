a0c1dc5cd07efdb5b55d2163a6499736543f3727
Mihretthe <mihretekalgn@gmail.com>
2026-05-14 17:13:21 +0300

    Fix namespace casing for InvoiceResource in CustomerResource

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Resources\V1;
 
-use App\Http\Resources\v1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
